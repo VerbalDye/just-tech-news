d76f32232cb3f93d996ca6ac3917d57d998ee118
Dustin Huntsberger <dhunts258@gmail.com>
2022-05-06 16:53:59 -0400

    initial setup

---
+++ routes/api/user-routes.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+// GET /api/users
+router.get('/', (req, res) => {});
+
+router.get('/:id', (req, res) => {});
