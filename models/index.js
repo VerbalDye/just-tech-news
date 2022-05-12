70a456253a9493c431fe823535a7ba290570075b
Dustin Huntsberger <dhunts258@gmail.com>
2022-05-12 15:18:22 -0500

    added vote model

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const User = require('./User');
 const Post = require('./Post');
+const Vote = require('./Vote');
 
 User.hasMany(Post, {
     foreignKey: 'user_id'
@@ -9,6 +10,16 @@ Post.belongsTo(User, {
     foreignKey: 'user_id'
 });
 
+User.belongsToMany(Post, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'user_id'
+});
 
+Post.belongsToMany(User, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'post_id'
+});
 
 module.exports = { User, Post };
