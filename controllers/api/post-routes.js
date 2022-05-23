dfdc89f00e7fe271a7299e77ed24993f8376d404
Dustin Huntsberger <dhunts258@gmail.com>
2022-05-23 15:54:40 -0400

    added semicolon to return

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -59,7 +59,7 @@ router.get('/:id', (req, res) => {
         .then(dbPostData => {
             if (!dbPostData) {
                 res.status(404).json({ message: 'No post found with this id' });
-                return
+                return;
             }
             res.json(dbPostData);
         })
